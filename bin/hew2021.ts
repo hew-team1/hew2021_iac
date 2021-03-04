2440751210fd7c0595d56b0b2469ef2bfa85e8ee
raityupiyo <42093052+piyoraik@users.noreply.github.com>
2021-03-05 02:23:55 +0900

    FrontStack(未確認)

--- bin/hew2021.ts
+++ bin/hew2021.ts
@@ -4,8 +4,10 @@ import * as cdk from '@aws-cdk/core';
 import { BaseStack } from '../lib/base-stack';
 import { DynamoDBStack } from '../lib/dynamodb-stack';
 import { BackendStack } from '../lib/backend-stack';
+import { FrontendStack } from '../lib/frontend-stack';
 
 const app = new cdk.App();
 const basestack = new BaseStack(app, 'BaseStack');
 new BackendStack(app, "BackendStack", basestack.HEW2021_Cluster, basestack.raityupiyodev);
+new FrontendStack(app, "FrontendStack",basestack.HEW2021_Cluster, basestack.raityupiyodev, basestack.certificate)
 new DynamoDBStack(app, 'DynamoDBStack');
--- lib/frontend-stack.ts
+++ lib/frontend-stack.ts
@@ -1,9 +1,56 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from '@aws-cdk/core'
+import { ICluster } from '@aws-cdk/aws-ecs';
+import { ICertificate } from '@aws-cdk/aws-certificatemanager';
+import { IHostedZone } from '@aws-cdk/aws-route53';
+import * as ecr from '@aws-cdk/aws-ecr';
+import * as ecs from '@aws-cdk/aws-ecs';
+import * as alb from "@aws-cdk/aws-ecs-patterns";
+import * as elbv2 from "@aws-cdk/aws-elasticloadbalancingv2"
 
-export class Hew2021Stack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+const env = {
+  region: process.env.CDK_DEFAULT_REGION,
+  account:process.env.CDK_DEFAULT_ACCOUNT
+}
+export class FrontendStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string,HEW2021_Cluster: ICluster, raityupiyodev: IHostedZone, certificate:ICertificate , props?: cdk.StackProps) {
+    super(scope, id, props ? props : { env });
 
     // The code that defines your stack goes here
+    //? Front Service
+    //! Front Repository
+    const FrontRepository = ecr.Repository.fromRepositoryName(this,
+      "hew_front",
+      "hew_front"
+    )
+
+    //! Front Task Create
+    const FrontTask      = new ecs.FargateTaskDefinition(this, "FrontTask")
+    const FrontContainer = FrontTask.addContainer("FrontContainer", {
+      image: ecs.ContainerImage.fromEcrRepository(FrontRepository, "v1.0.6"),
+      logging: ecs.LogDrivers.awsLogs({
+        streamPrefix: "FrontLogs"
+      })
+    })
+    FrontContainer.addPortMappings({
+      hostPort: 80,
+      containerPort: 80
+    })
+
+    const FrontALB = new alb.ApplicationLoadBalancedFargateService(this, "FrontService", {
+      cluster: HEW2021_Cluster,
+      memoryLimitMiB: 256,
+      cpu: 256,
+      desiredCount: 1,
+      taskDefinition: FrontTask,
+      protocol: elbv2.ApplicationProtocol.HTTPS,
+      redirectHTTP: true,
+      certificate: certificate,
+      domainName: "raityupiyo.dev",
+      domainZone: raityupiyodev,
+    })
+    FrontALB.targetGroup.configureHealthCheck({
+      path: '/',
+      port: '80'
+    })
   }
 }
